a2c2b421716020adbd4516c4df732043ed43ea18
Alejandro Figueroa <aleckzf@gmail.com>
2026-07-21 20:09:22 -0600

    Added Preact Greeting dynamic island to the hoe page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-blog-demo-site.netlify.app"
+  site: "https://astro-blog-demo-site.netlify.app",
+  integrations: [preact()]
 });
